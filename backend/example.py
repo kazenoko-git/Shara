1e6ee37ac76d2550f691251c2320b237d49bd665
Abby <130657335+Delirium157@users.noreply.github.com>
2025-12-13 06:04:26 +0530

    It works now, I guess :D
    Added functionality to save inference results as JSON files in the output folder.

--- backend/example.py
+++ backend/example.py
@@ -2,6 +2,9 @@ import json
 import requests
 import os
 
+# NOTE: REQUIRES AN 'images' FOLDER AND 'output' FOLDER IN SAME DIRECTORY
+# WILL MAKE API CALLS TO RUN INFERENCE ON EACH IMG IN images AND SAVE JSON TO OUTPUT
+
 # Run inference on an image
 url = "https://predict.ultralytics.com"
 headers = {"x-api-key": "..."} # IVAN API KEY GOES HERE 
@@ -10,10 +13,12 @@ data = {"model": "https://hub.ultralytics.com/models/7j3uWTMc5oTCmiUkbzCx", "img
 images = os.listdir("images/")
 for image in images:
     with open(f"images/{image}", "rb") as f:
-	    response = requests.post(url, headers=headers, data=data, files={"file": f})
+        response = requests.post(url, headers=headers, data=data, files={"file": f})
 
-# Check for successful response
-response.raise_for_status()
+        response.raise_for_status()
 
-# Print inference results
-print(json.dumps(response.json(), indent=2))
+        # SAVE JSON
+        with open(f"output/{image}.json", "w") as j:
+            j.write(json.dumps(response.json(), indent=2))
+
+input("Done - Press Enter and check output folder.")
